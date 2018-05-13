7d78044bd651b6e98a39f66551ac1b5fb5ba8129
Villa.Cr <30905769+villa97@users.noreply.github.com>
2018-05-13 16:01:03 -0500

    Variables globales y configuracion de puertos.
    Se crearon las variables globales necesarias y se configuraron los puertos para el modo de trabajo.

--- main.c
+++ main.c
@@ -8,6 +8,11 @@
 #use fast_io(c)
 #use fast_io(d)
 #use fast_io(e)
+//Variables globales.
+long resultadoPrimerConversion = 0;
+long resultadoSegundaConversion = 0;
+short primerConversion = 0;
+short segundaConversion = 0;
 //interrupcion del ADC
 #int_AD
 void ISR_ADC(void)
@@ -28,6 +33,15 @@ void main (void)
    setup_adc_ports(AN0_TO_AN1);
    //Interrupciones.
    enable_interrupts(INT_AD);
-   enable_interrupts(GLOBAL); 
-   
+   enable_interrupts(GLOBAL);  
+   //puertos.
+   set_tris_a(0x03);
+   set_tris_b(0x00);
+   set_tris_c(0x00);
+   set_tris_d(0x00);
+   set_tris_e(0x00);
+   while(1)
+   { 
+     
+   }
 }
