799f6bb58e1fa69bc1d06a3149c693d7353aa9d9
Villa.Cr <30905769+villa97@users.noreply.github.com>
2018-05-13 15:57:34 -0500

    Configuracion de ADC e interrupcoion.
    Se agrega la configuracion necesaria para el funcionamiento del ADC.
    Se implementa el funcionamiento de la interrupcion.

--- main.c
+++ main.c
@@ -1,9 +1,33 @@
 #include <18F4620.h>
+#device adc=10 
 #fuses HS, NOFCMEN, NOIESO, PUT, NOBROWNOUT, NOWDT
 #fuses NOPBADEN, NOMCLR, STVREN, NOLVP, NODEBUG
 #use delay(clock=16000000)
-void main (void)
+#use fast_io(a)
+#use fast_io(b)
+#use fast_io(c)
+#use fast_io(d)
+#use fast_io(e)
+//interrupcion del ADC
+#int_AD
+void ISR_ADC(void)
 {
- 
+    if(primerConversion == 1)
+       resultadoPrimerConversion = read_adc(ADC_READ_ONLY);
+    if(segundaConversion == 1)
+        resultadoSegundaConversion = read_adc(ADC_READ_ONLY);
+   read_adc(ADC_START_ONLY);
 }
 
+//Funcion principal
+void main (void)
+{
+   setup_oscillator(OSC_16MHZ);
+   //Configuracion del ADC
+   setup_adc(ADC_CLOCK_DIV_32);
+   setup_adc_ports(AN0_TO_AN1);
+   //Interrupciones.
+   enable_interrupts(INT_AD);
+   enable_interrupts(GLOBAL); 
+   
+}
